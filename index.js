1fc9aa8e93a0ec4aaea675191fba0160370246cd
Mercy Castro <mercy.castro@correounivalle.edu.co>
2023-05-10 18:07:44 -0500

    backend

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+    return res.json({
+        message:err
+    });
+});
+
+const port = 3000
+
+app.listen(port);
+
+console.log('Server on port' + port);

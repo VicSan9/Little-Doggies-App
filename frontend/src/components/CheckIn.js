1790c7c311aa6f023f9bdbf779a44465723271c2
Mercy09 <mercy.castro@correounivalle.edu.co>
2023-05-24 20:55:51 -0500

    update

--- frontend/src/components/CheckIn.js
+++ frontend/src/components/CheckIn.js
@@ -1,37 +1,37 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography,  CardContent, TextField, Button } from "@mui/material";
 
 export default function CheckIn() {
     return (
       <Container maxWidth='lg'>
           <Grid container
                 spacing='2'
-                direction=''
+                direction='row'
                 justifyContent='center'
                 alignItems="flex-start"
-                margin='140px 150px 100px 150px' 
+                margin='140px 160px 100px 150px' 
                 width='auto' 
                 maxHeight='70vh' 
                 height='75vw'
                 sx={{border:'2px solid #BABBBF', borderRadius:'20px'}}>
-              <Grid item xs='12' sm='12' lg='6' md='6' xl='6'
+              <Grid item xs='2' sm='2' lg='2' md='2' xl='3'
                     sx={{borderRadius:'19px 0px 0px 19px'}}>
-                <Grid direction= 'colummn' mt='2vw' variant='h2' fontWeight='bold'>
-                  <Grid items xs= '12' sm='12' lg='6' md='6' xl='6'>
-                    <Typography variant="body1" fontWeight='bold' fontSize='1vw'>
-                        Crea tu cuenta
-                    </Typography>
+                  <Grid direction= 'row' mt='2vw' variant='h2' fontWeight='bold'>
+                    <Grid items xs= '2' sm='2' lg='2' md='2' xl='3'>
+                      <Typography variant="body1" fontWeight='bold' fontSize='1vw'>
+                      Crea tu cuenta
+                      </Typography>
+                    </Grid>
                   </Grid>
-                  </Grid>
-                  <Grid direction= 'column' mt='2vw' variant='h2'>
-                    <Grid items xs='12' sm='12' lg='6' md='6' xl='6' >
+              <Grid direction= 'row' mt='2vw' variant='h2'>
+                    <Grid items xs='2' sm='2' lg='2' md='2' xl='3' >
                     <Typography variant="body1" fontSize='1vw'>
-                        Registrate para poder acceder a nuestros servicios.
+                      Registrate para poder acceder a nuestros servicios.
                     </Typography>
                     </Grid>
-                  </Grid>  
-                </Grid>
-              </Grid>
-      </Container>
+              </Grid>  
+            </Grid>
+          </Grid>
+        </Container>
       
     )
   }

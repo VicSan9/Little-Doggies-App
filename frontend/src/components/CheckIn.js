1b1b54c3b189ac4bcb5515c77001e9dad2f675b4
Mercy09 <mercy.castro@correounivalle.edu.co>
2023-05-26 15:48:05 -0500

    Update CheckIn

--- frontend/src/components/CheckIn.js
+++ frontend/src/components/CheckIn.js
@@ -4,17 +4,18 @@ export default function CheckIn() {
     return (
       <Container maxWidth='lg'>
           <Grid container
-                spacing='1'
+                spacing='2'
                 direction='row'
-                justifyContent='center'
-                alignItems="flex-start"
+                justifyContent='flex-start'
+                alignItems="center"
                 margin='140px 160px 100px 150px' 
                 width='auto' 
                 maxHeight='70vh' 
                 height='65vw'
-                sx={{border:'2px solid #BABBBF', borderRadius:'20px'}}>
-              <Grid style={{padding: "80px, 5px 0 spx"}} mt='2vw'>
-                      <Typography variant="h4" fontWeight='bold' fontSize='1vw' align="center">
+                sx={{
+                  border:'2px solid #BABBBF', borderRadius:'20px'}}>
+              <Grid style={{padding: "80px, 5px 0 5px"}} >
+                      <Typography variant="h5" fontWeight='bold' align="center">
                         Crea tu cuenta
                       </Typography>
                       <Typography variant="subtitle1" fontSize='1vw' mt='1vw' align="center">
@@ -22,69 +23,50 @@ export default function CheckIn() {
                       </Typography>
                       <form>
                         <Grid container spacing='1' mt='2vw'>
-                          <Grid xs='12' sm='6'  lg='6' md='6' xl='6' item>
-                          <TextField 
-                            placeholder="Ingrese sus nombres" 
-                            label="Nombres" 
-                            variant="outlined" 
-                            fullWidth required>
+                          <Grid xs='12' sm='12'  lg='12' md='12' xl='12' item>
+                          <TextField id="outlined-basic" 
+                            label="Nombres"
+                            variant="outlined"
+                            sx={{ ml: '30px', mt:'1vw'}}>
                           </TextField>
-                          </Grid>
-                          <Grid xs='12' sm='6'  lg='6' md='6' xl='6' item>
-                          <TextField 
-                            placeholder="Ingrese sus apellidos" 
-                            label="Apellidos" 
-                            variant="outlined" 
-                            fullWidth required>
+                          <TextField id="outlined-basic"
+                            label="Apellidos"
+                            variant="outlined"
+                            sx={{ ml: '30px', mt:'1vw'}}>
                           </TextField>
-                          </Grid>
-                          <Grid xs='12' sm='6'  lg='6' md='6' xl='6' mt='1vw' item>
-                          <TextField 
-                            placeholder="Celular" 
-                            label="Celular" 
-                            variant="outlined" 
-                            fullWidth required>
+                          <TextField id="outlined-basic"
+                            label="Telefono"
+                            variant="outlined"
+                            sx={{ ml: '30px', mt:'1vw'}}>
                           </TextField>
-                          </Grid> 
-                          <Grid xs='12' sm='6'  lg='6' md='6' xl='6' mt='1vw'  item>
-                          <TextField 
-                            placeholder="Correo Electronico" 
-                            label="Correo" 
-                            variant="outlined" 
-                            fullWidth required>
+                          <TextField id="outlined-basic"
+                            label="Correo Electrónico"
+                            variant="outlined"
+                            sx={{ ml: '30px', mt:'1vw'}}>
                           </TextField>
-                          </Grid> 
-                          <Grid xs='12' sm='6'  lg='6' md='6' xl='6' mt='1vw' borderBottomitem>
-                          <TextField 
-                            placeholder="Usuario" 
-                            label="Usuario" 
-                            variant="outlined" 
-                            fullWidth required>
+                          <TextField id="outlined-basic"
+                            label="Usuario"
+                            variant="outlined"
+                            sx={{ ml: '30px', mt:'1vw'}}>
                           </TextField>
-                          </Grid> 
-                          <Grid xs='12' sm='6'  lg='6' md='6' xl='6' mt='1vw' item>
-                          <TextField 
-                            placeholder="Contraseña" 
-                            label="Contraseña" 
-                            variant="outlined" 
-                            fullWidth required>
+                          <TextField id="outlined-basic"
+                            label="Contraseña"
+                            variant="outlined"
+                            sx={{ ml: '30px', mt:'1vw'}}>
                           </TextField>
-                          </Grid> 
-                          <Grid xs='12' sm='6'  lg='6' md='6' xl='6' mt='1vw' item>
-                          <TextField 
-                            placeholder="Dirección" 
-                            label="Dirección" 
-                            variant="outlined" 
-                            fullWidth required>
+                          <TextField id="outlined-basic"
+                            label="Direccion"
+                            variant="outlined"
+                            sx={{ ml: '30px', mt:'1vw'}}>
                           </TextField>
-                          </Grid> 
-                          <Button variant="contained" m='1vw'
+                          <Button variant="contained" 
                             size='small'
                             sx={{
                             color: '#0265CD',
                             width: '200px',
                             height: '45px',
-                            mt: '50px',
+                            mt: '20px',
+                            ml: '30px',
                             borderColor: '#0265CD',
                             borderRadius: '15px'
                                }}>
@@ -98,6 +80,7 @@ export default function CheckIn() {
                           </Link>
                           </Button>
                         </Grid>
+                      </Grid>
                       </form>
                   </Grid>
               </Grid>

fdd74412d1acb685afce2f0e80a5873047dc790a
Mercy09 <mercy.castro@correounivalle.edu.co>
2023-06-18 10:02:01 -0500

    Update Shoppinghistory

--- backend/src/routes/orders.routes.js
+++ backend/src/routes/orders.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
-const { getAllOrders, getOrder, createOrder, deleteOrder, updateOrder } = require('../controllers/orders.controller');
+const { getAllOrders, getOrder, createOrder, deleteOrder, updateOrder, getOrder2 } = require('../controllers/orders.controller');
 
 const router = Router();
 
 router.get('/orders', getAllOrders)
 
 router.get('/orders/:id', getOrder)
+
+router.get('/orders2', getOrder2)
 
 router.post('/orders', createOrder)
 

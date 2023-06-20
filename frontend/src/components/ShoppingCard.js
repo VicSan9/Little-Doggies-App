2723aebb141d055a09c08213bacb984e70d17352
Víctor Alfonso Sánchez Lasso <svictorvf910@gmail.com>
2023-06-19 21:23:24 -0500

    Create ShoppingCard.js

---
+++ frontend/src/components/ShoppingCard.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+import Navbar from "./Navbar";
+
+export default function ShoppingCart() {
+    return (
+      <><Navbar></Navbar>
+      <Typography mt='100px'>Carrito de compras</Typography></>
+    )
+}

6c8b38cf3bbaeede8bd907694a170480a25841df
Víctor Alfonso Sánchez Lasso <svictorvf910@gmail.com>
2023-06-22 18:38:31 -0500

    Update AdminQuotes.js

--- frontend/src/components/AdminQuotes.js
+++ frontend/src/components/AdminQuotes.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import AdminNavbar from './AdminNavbar'
+import { Grid } from "@mui/material";
 
-export default function AdminQuuotes () {
-  return (
-    <div>Sumadre</div>
-  )
+export default function AdminQuotes() {
+    return (
+        <>
+            <Grid
+                container
+                direction='row'
+                width='100vw'
+                height='100vh'
+                justifyContent='start'
+                alignItems='center'>
+                <AdminNavbar></AdminNavbar>
+                <div>Citas admin</div>
+            </Grid>
+        </>
+    )
 }

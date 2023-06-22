f4c2a17751f3ee2a1214445171aba05b025cfe83
Mercy09 <mercy.castro@correounivalle.edu.co>
2023-06-21 19:01:14 -0500

    Revert "Revert "Revert "Update Recover"""
    This reverts commit ba822602fef043b115f6b52125a178e7d7d59068.

--- frontend/src/components/Recover.js
+++ frontend/src/components/Recover.js
@@ -1,6 +1,6 @@
 import { Container, Grid, Typography, TextField, Button, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { useState} from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import emailjs from "@emailjs/browser";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -9,15 +9,19 @@ export default function Recover() {
 
   const navigate = useNavigate()
 
-  const [client, setClient] = useState([])
+  var aleatorio = Math.round(Math.random() * 999999);
 
-  const [recover, setRecover] = useState({ userEmail: '' });
+  const [user, setUser] = useState([])
+
+  const [code, setCode] = useState({ codigo: ' ' })
+
+  const [recover, setRecover] = useState({ userEmail: '', emailTitle: 'Correo de recuperación', emailDetails: aleatorio, to_name: user.nombres + " " + user.apellidos });
 
   const [errorMessage, setErrorMessage] = useState("");
 
-  const [successMessage, setSuccessMessage] = useState("");
+  const [isHidden, setIsHidden] = useState(false)
 
-  const loadClient = async () => {
+  const loadUser = async () => {
 
     const correo = recover.userEmail
 
@@ -32,55 +36,67 @@ export default function Recover() {
     const data = await res.json();
 
     if (res.status === 404) {
-      setClient([])
+      setUser([])
       return
     }
 
-    setClient(data)
+    setUser(data)
   }
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
 
   const handleChange = e => {
     setRecover({
       ...recover,
       [e.target.name]: e.target.value
     })
+    console.log(recover)
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    loadClient();
-
-    if (recover.userEmail.trim() === '' ) {
-      setErrorMessage("Ingrese el correo");
-      setRecover({ userEmail: '', })
-      return
-    }
-
     emailjs.send(
       "service_y2xbs55", "template_52cw0cs", {
-      to_name: client.nombres,
+      to_name: recover.to_name,
       from_name: "Little Doggies",
-      message: client.contraseña,
+      message: recover.emailDetails,
       userEmail: recover.userEmail,
       reply_to: "doggieslittle0@gmail.com",
-    }, "6r2SOp-a0vpthnTN_")
-
-    setSuccessMessage("Envio exitoso")
+    }, "6r2SOp-a0vpthnTN_").then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+        return
+      }
+    );
+    setIsHidden(true)
   }
 
   const handleClick = () => {
     setErrorMessage("");
   }
 
-  const handleClick1 = () => {
-    setSuccessMessage("");
-    navigate('/login')
-  }
-
   const handleCancel = () => {
     navigate('/login')
   }
+
+  const handleCancel2 = () => {
+    setIsHidden(false)
+  }
+
+  const handleContinue = () => {
+    if (recover.aleatorio === code.codigo) {
+      navigate('/contraseña')
+    } else {
+      setErrorMessage("Codigo incorrecto")
+    }
+  };
 
 
   const ErrorComponent = ({ errorMessage }) => {
@@ -118,156 +134,214 @@ export default function Recover() {
     );
   };
 
-  const SuccessComponent1 = ({ successMessage }) => {
-    return (
-      <Grid container
-        zIndex='2'
-        width='100vw'
-        height='100vh'
-        position='absolute'
-        alignItems='center'
-        textAlign='center'
-        justifyContent='center'
-        sx={{ backgroundColor: 'rgba(0,0,0,.2)', backdropFilter: 'blur(5px)', }}>
-        <Box
-          width='300px'
-          height='200px'
-          borderRadius='20px'
-          border='1px solid #BABBBF'
-          sx={{ backgroundColor: '#ffffff' }}>
-          <Typography color='#22E94E' mt='20px' variant="h5" fontWeight='bold'>Mensaje</Typography>
-          <p>{successMessage}</p>
-          <Button variant="outlined"
-            size='medium'
-            onClick={handleClick1}
-            sx={{
-              color: '#0265CD',
-              mt: '30px',
-              borderColor: '#0265CD',
-              borderRadius: '50px',
-              textTransform: 'none'
-            }}> Volver
-          </Button>
-        </Box>
-      </Grid>
-    );
-  };
-
   return (
     <>
       {errorMessage && <ErrorComponent errorMessage={errorMessage} />}
-      {successMessage && <SuccessComponent1 successMessage={successMessage} />}
       <Navbar></Navbar>
       <Container maxWidth='lg' fixed>
-        <Grid
-          container
-          height='100vh'
-          alignItems='center'
-          justifyContent='center'
-          minHeight='700px' >
+        <div hidden={isHidden}>
           <Grid
             container
-            direction='column'
+            height='100vh'
             alignItems='center'
             justifyContent='center'
-            border='1px solid #BABBBF'
-            borderRadius='20px'
-            height='500px'
-            width='470px'
-            sx={{ display: { xs: 'none', sm: 'none', md: 'flex' } }}>
-            <LockOutlinedIcon sx={{ fontSize: '67px' }}></LockOutlinedIcon>
-            <Typography
-              mt='30px'
-              variant="h5"
-              fontWeight='bold'>¿Tienes problemas para ingresar?
-            </Typography>
-            <Typography
-              textAlign='center'
-              mt='30px'
-              mb='30px'
-              variant="body1">Ingresa tu correo y te enviaremos tu contraseña.
-            </Typography>
-            <Grid component={'form'} onSubmit={handleSubmit}>
-              <TextField
-                name="userEmail"
-                type="email"
-                label="Correo"
-                variant="outlined"
-                value={recover.userEmail}
-                onChange={handleChange}
-                sx={{ width: '400px' }}
-              >
-              </TextField>
-              <Grid container mt='30px' direction='row' alignItems='center' justifyContent='center'>
-                <Button
-                  onClick={handleCancel}
-                  variant="contained"
-                  sx={{ backgroundColor: "#BABBBF", borderRadius: '50px', width: '130px' }}>Cancelar
-                </Button>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  sx={{ ml: '20px', borderRadius: '50px', width: '130px' }}>Continuar
-                </Button>
+            minHeight='700px' >
+            <Grid
+              container
+              direction='column'
+              alignItems='center'
+              justifyContent='center'
+              border='1px solid #BABBBF'
+              borderRadius='20px'
+              height='500px'
+              width='470px'
+              sx={{ display: { xs: 'none', sm: 'none', md: 'flex' } }}>
+              <LockOutlinedIcon sx={{ fontSize: '67px' }}></LockOutlinedIcon>
+              <Typography
+                mt='30px'
+                variant="h5"
+                fontWeight='bold'>¿Tienes problemas para ingresar?
+              </Typography>
+              <Typography
+                textAlign='center'
+                mt='30px'
+                mb='30px'
+                variant="body1">Ingresa tu correo y te enviaremos un código de recuperación.
+              </Typography>
+              <Grid component={'form'} onSubmit={handleSubmit}>
+                <TextField
+                  name="userEmail"
+                  type="email"
+                  label="Correo"
+                  variant="outlined"
+                  value={recover.userEmail}
+                  onChange={handleChange}
+                  sx={{ width: '400px' }}
+                >
+                </TextField>
+                <Grid container mt='30px' direction='row' alignItems='center' justifyContent='center'>
+                  <Button
+                    onClick={handleCancel}
+                    variant="contained"
+                    sx={{ backgroundColor: "#BABBBF", borderRadius: '50px', width: '130px' }}>Cancelar
+                  </Button>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ ml: '20px', borderRadius: '50px', width: '130px' }}>Continuar
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction='column'
+              alignItems='center'
+              justifyContent='center'
+              borderRadius='20px'
+              sx={{ display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', sx: 'none' } }}>
+              <LockOutlinedIcon sx={{ fontSize: '67px' }}></LockOutlinedIcon>
+              <Typography
+                textAlign='center'
+                mt='30px'
+                mr='10px'
+                ml='10px'
+                variant="h5"
+                fontWeight='bold'>¿Tienes problemas para ingresar?
+              </Typography>
+              <Typography
+                textAlign='center'
+                mt='30px'
+                mr='10px'
+                ml='10px'
+                mb='30px'
+                variant="body1">Ingresa tu correo y te enviaremos un código de recuperación.
+              </Typography>
+              <Grid component={'form'} onSubmit={handleSubmit}>
+                <TextField
+                  name="correo"
+                  type="email"
+                  label="Correo"
+                  variant="outlined"
+                  value={recover.userEmail}
+                  onChange={handleChange}
+                  sx={{ width: '80vw', maxWidth: '480px' }}>
+                </TextField>
+                <Box sx={{ height: '30px', width: '30px' }}></Box>
+                <Grid
+                  container
+                  direction='row'
+                  alignItems='center'
+                  justifyContent='center'
+                  sx={{ display: { xs: 'contents', sm: ' flex' } }}>
+                  <Button
+                    onClick={handleCancel}
+                    variant="contained"
+                    sx={{ backgroundColor: "#BABBBF", borderRadius: '50px', width: '130px', ml: '20vw' }}>Cancelar
+                  </Button>
+                  <Box sx={{ height: '30px', width: '30px' }}></Box>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ borderRadius: '50px', width: '130px', ml: '20vw' }}>Continuar
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Grid
-            container
-            direction='column'
-            alignItems='center'
-            justifyContent='center'
-            borderRadius='20px'
-            sx={{ display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', sx: 'none' } }}>
-            <LockOutlinedIcon sx={{ fontSize: '67px' }}></LockOutlinedIcon>
-            <Typography
-              textAlign='center'
-              mt='30px'
-              mr='10px'
-              ml='10px'
-              variant="h5"
-              fontWeight='bold'>¿Tienes problemas para ingresar?
-            </Typography>
-            <Typography
-              textAlign='center'
-              mt='30px'
-              mr='10px'
-              ml='10px'
-              mb='30px'
-              variant="body1">Ingresa tu correo y te enviaremos tu contraseña.
-            </Typography>
-            <Grid component={'form'} onSubmit={handleSubmit}>
-              <TextField
-                name="correo"
-                type="email"
-                label="Correo"
-                variant="outlined"
-                value={recover.userEmail}
-                onChange={handleChange}
-                sx={{ width: '80vw', maxWidth: '480px' }}>
-              </TextField>
-              <Box sx={{ height: '30px', width: '30px' }}></Box>
-              <Grid
+        </div>
+        <div hidden={!isHidden}>
+          <Navbar></Navbar>
+          <Container maxWidth='lg' fixed>
+            <Grid container
+              height='100vh'
+              alignItems='center'
+              justifyContent='center'
+              minHeight='700px' >
+              <Grid component={'form'} onSubmit={handleSubmit}
                 container
-                direction='row'
+                direction='column'
                 alignItems='center'
                 justifyContent='center'
-                sx={{ display: { xs: 'contents', sm: ' flex' } }}>
-                <Button
-                  onClick={handleCancel}
-                  variant="contained"
-                  sx={{ backgroundColor: "#BABBBF", borderRadius: '50px', width: '130px', ml: '20vw' }}>Cancelar
-                </Button>
-                <Box sx={{ height: '30px', width: '30px' }}></Box>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  sx={{ borderRadius: '50px', width: '130px', ml: '20vw' }}>Continuar
-                </Button>
+                border='1px solid #BABBBF'
+                borderRadius='20px'
+                height='350px'
+                width='450px'
+                sx={{ display: { xs: 'none', sm: 'none', md: 'flex' } }}>
+                <Typography
+                  variant="h5"
+                  fontWeight='bold'>Escribe tu código
+                </Typography>
+                <Typography
+                  mt='30px'
+                  mb='30px'
+                  textAlign='center'
+                  variant="body1">Te hemos enviado un código de recuperación al correo
+                </Typography>
+                <TextField
+                  name="codigo"
+                  label="codigo de verificación"
+                  variant="outlined"
+                  onChange={handleChange}
+                  sx={{ width: '400px' }}>
+                </TextField>
+                <Grid container mt='30px' direction='row' alignItems='center' justifyContent='center'>
+                  <Button
+                    onClick={handleCancel2}
+                    variant="contained"
+                    sx={{ backgroundColor: "#BABBBF", borderRadius: '50px', width: '130px' }}>Cancelar
+                  </Button>
+                  <Button
+                    onClick={handleContinue}
+                    type="submit"
+                    variant="contained"
+                    value={code.codigo}
+                    sx={{ ml: '20px', borderRadius: '50px', width: '130px' }}>Continuar
+                  </Button>
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                direction='column'
+                alignItems='center'
+                justifyContent='center'
+                borderRadius='20px'
+                sx={{ display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', sx: 'none' } }}>
+                <Typography
+                  textAlign='center'
+                  variant="h5"
+                  fontWeight='bold'>Escribe tu código
+                </Typography>
+                <Typography
+                  mt='30px'
+                  mb='30px'
+                  textAlign='center'
+                  variant="body1">Te hemos enviado un código de recuperación al correo
+                </Typography>
+                <TextField
+                  name="Código"
+                  label="Código de verificación"
+                  variant="outlined"
+                  sx={{ width: '80vw', maxWidth: '400px' }}>
+                </TextField>
+                <Grid container mt='30px' direction='row' alignItems='center' justifyContent='center'>
+                  <Button
+                    onClick={handleCancel2}
+                    variant="contained"
+                    sx={{ backgroundColor: "#BABBBF", borderRadius: '50px', width: '130px' }}>Cancelar
+                  </Button>
+                  <Button
+                    onClick={handleContinue}
+                    type="submit"
+                    variant="contained"
+                    value={code.codigo}
+                    sx={{ ml: '20px', borderRadius: '50px', width: '130px' }}>Continuar
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Grid>
+          </Container>
+        </div>
       </Container>
     </>
   )

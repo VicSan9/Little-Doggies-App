acef483517b9b94730cbead0c1f07ccb8a9dd2cc
Víctor Alfonso Sánchez Lasso <svictorvf910@gmail.com>
2023-06-22 22:44:57 -0500

    Update AdminNavbar.js

--- frontend/src/components/AdminNavbar.js
+++ frontend/src/components/AdminNavbar.js
@@ -5,23 +5,29 @@ export default function AdminNavbar() {
     return (
         <Grid
             container
-            width='83px'
+            width='5.5vw'
             height='100vh'
             justifyContent='center'
             alignItems='center'
-            maxHeight='930px'>
+            maxHeight='930px'
+            maxWidth='83px'
+            minWidth='50px'>
             <Grid
                 container
                 width='100%'
                 height='85%'
-                justifyContent='center'
+                justifyContent='start'
                 alignItems='center'
                 borderRight='1px solid #BABBBF'
                 borderRadius='10px'
-                direction='column'>
+                direction='column'
+                textAlign='center'>
                 <Grid
                     item xs={1} sm={1} lg={1} md={1} xl={1}>
-                    <Typography>AAA</Typography>
+                    <img src={process.env.PUBLIC_URL + "/LDLogo.png"}
+                        alt="logo"
+                        width='75%'>
+                    </img>
                 </Grid>
                 <Grid
                     item xs={1} sm={1} lg={1} md={1} xl={1}>

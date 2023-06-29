cb0689b008a96faa5162236574082f6329747566
Víctor Alfonso Sánchez Lasso <svictorvf910@gmail.com>
2023-06-28 19:32:49 -0500

    Update AdminClient.js

--- frontend/src/components/AdminClient.js
+++ frontend/src/components/AdminClient.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import AdminNavbar from './AdminNavbar'
-import { Grid, Typography, IconButton, Backdrop, Button, Box, Card } from "@mui/material";
+import { Grid, Typography, IconButton, Backdrop, Button, Box, Card, Avatar, Divider, CardContent } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
@@ -8,9 +8,11 @@ export default function AdminClient() {
 
     const [open, setOpen] = useState(false);
     const [open1, setOpen1] = useState(false);
+    const [isHidden, setIsHidden] = useState(false)
     const [isHidden1, setIsHidden1] = useState(false)
     const [clients, setClients] = useState([])
     const [client, setClient] = useState([])
+    const [pets, setPets] = useState([])
     const [errorMessage, setErrorMessage] = useState("");
     const [advertenceMenssage, setAdvertenceMenssage] = useState("");
 
@@ -83,6 +85,7 @@ export default function AdminClient() {
         const id = Number(e.currentTarget.id)
 
         setIsHidden1(true)
+        setIsHidden(false)
 
         const res = await fetch(`http://localhost:4000/clients/${id}`, {
             method: 'GET',
@@ -93,7 +96,25 @@ export default function AdminClient() {
 
         setClient(data)
 
-        console.log(data)
+        const body = {
+            'id': data.clid
+        }
+
+        const res2 = await fetch(`http://localhost:4000/pets`, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: { "content-Type": "application/json" }
+        })
+
+        if (res2.status === 404) {
+            setIsHidden(true)
+            setPets([])
+            return
+        }
+
+        const data2 = await res2.json()
+
+        setPets(data2)
     }
 
     const ErrorComponent = ({ errorMessage }) => {
@@ -333,7 +354,8 @@ export default function AdminClient() {
                                                 cursor: 'pointer'
                                             }
                                         }}>
-                                        <Typography>{client.nombres}</Typography>
+                                        <Avatar sx={{ width: 45, height: 45 }}></Avatar>
+                                        <Typography textAlign='center' width='62%' overflow='hidden'>{client.nombres + ' ' + client.apellidos}</Typography>
                                         <Grid>
                                             <IconButton id={client.clid} onClick={handleClickEdit} sx={{ width: '30px', height: '30px', ":hover": { color: "white" } }}>
                                                 <EditIcon></EditIcon>
@@ -356,12 +378,136 @@ export default function AdminClient() {
                         paddingRight='16px'
                         paddingLeft='16px'
                         item xs={8} sm={8} lg={8} md={8} xl={8}>
-                        <div hidden={isHidden1}>
-                            <Typography mt='8px' mb='8px'>Seleciona uno de los clientes para ver su información</Typography>
-                        </div>
-                        <div hidden={!isHidden1} style={{ width: '100%' }}>
-                            <Typography variant='h6'>Información del Cliente</Typography>
-                        </div>
+                        <Grid
+                            container
+                            height='100%'
+                            overflow='scroll'
+                            alignItems='center'
+                            justifyContent='start'
+                            display='block'
+                            direction='column'
+                            sx={{
+                                '&::-webkit-scrollbar': {
+                                    width: '8px',
+                                    height: '8px',
+                                },
+                                '&::-webkit-scrollbar-thumb': {
+                                    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                                    borderRadius: '10px',
+                                    '&:hover': {
+                                        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                                    },
+                                },
+                                '&::-webkit-scrollbar: horizontal': {
+                                    display: 'none',
+                                },
+                            }}>
+                            <div hidden={isHidden1}>
+                                <Typography mt='8px' mb='8px'>Seleciona uno de los clientes para ver su información</Typography>
+                            </div>
+                            <div hidden={!isHidden1} style={{ width: '100%' }}>
+                                <Typography variant='h6'>Información del Cliente</Typography>
+                                <Typography mb='15px' mt='15px' ml='10px' variant='h6' width='98%' sx={{ fontSize: '18px' }}>Información del personal
+                                    <Grid
+                                        container
+                                        alignItems='start'
+                                        justifyContent='center'>
+                                        <Grid item xs={4} sm={4} lg={4} md={4} xl={4}>
+                                            <Typography ml='15px' mt='8px' variant='body1' fontWeight='500'>Nombres</Typography>
+                                            <Typography ml='15px' mt='8px' variant='body1' fontWeight='500'>Apellidos</Typography>
+                                        </Grid>
+                                        <Grid item xs={4} sm={4} lg={4} md={4} xl={4}>
+                                            <Typography mt='8px' variant='body1'>{client.nombres}</Typography>
+                                            <Typography mt='8px' variant='body1'>{client.apellidos}</Typography>
+                                        </Grid>
+                                        <Grid item xs={4} sm={4} lg={4} md={4} xl={4}>
+                                            <Avatar sx={{ width: 100, height: 100 }}></Avatar>
+                                        </Grid>
+                                    </Grid>
+                                </Typography>
+                                <Divider></Divider>
+                                <Typography mb='15px' mt='15px' ml='10px' variant='h6' width='98%' sx={{ fontSize: '18px' }}>Información de contacto
+                                    <Grid
+                                        container
+                                        alignItems='start'
+                                        justifyContent='center'>
+                                        <Grid item xs={4} sm={4} lg={4} md={4} xl={4}>
+                                            <Typography ml='15px' mt='8px' variant='body1' fontWeight='500'>Correo electrónico</Typography>
+                                            <Typography ml='15px' mt='8px' variant='body1' fontWeight='500'>Número de telefono</Typography>
+                                            <Typography ml='15px' mt='8px' variant='body1' fontWeight='500'>Dirección</Typography>
+                                        </Grid>
+                                        <Grid item xs={8} sm={8} lg={8} md={8} xl={8}>
+                                            <Typography mt='8px' variant='body1'>{client.correo}</Typography>
+                                            <Typography mt='8px' variant='body1'>{client.telefono}</Typography>
+                                            <Typography mt='8px' variant='body1'>{client.direccion}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Typography>
+                                <Divider></Divider>
+                                <Typography mb='15px' mt='15px' ml='10px' variant='h6' width='98%' sx={{ fontSize: '18px' }}>Información de la cuenta
+                                    <Grid
+                                        container
+                                        alignItems='start'
+                                        justifyContent='center'>
+                                        <Grid item xs={4} sm={4} lg={4} md={4} xl={4}>
+                                            <Typography ml='15px' mt='8px' variant='body1' fontWeight='500'>Nombre de usuario</Typography>
+                                            <Typography ml='15px' mt='8px' variant='body1' fontWeight='500'>Contraseña</Typography>
+                                        </Grid>
+                                        <Grid item xs={8} sm={8} lg={8} md={8} xl={8}>
+                                            <Typography mt='8px' variant='body1'>{client.usuario}</Typography>
+                                            <Typography mt='8px' variant='body1'>{client.contraseña}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Typography>
+                                <Divider></Divider>
+                                <Typography mb='15px' mt='15px' ml='10px' variant='h6' width='98%' sx={{ fontSize: '18px' }}>Mascotas
+                                    <div hidden={isHidden}>
+                                        <Grid
+                                            container
+                                            alignItems='start'
+                                            justifyContent='start'>
+                                            {pets.map((pet) => (
+                                                <Card
+                                                    component={Button}
+                                                    key={pet.mcid}
+                                                    value={pet.mcid}
+                                                    sx={{
+                                                        border: '1px solid #BABBBF',
+                                                        borderRadius: '10px',
+                                                        textTransform: 'none',
+                                                        mt: '15px',
+                                                        ml: '15px',
+                                                        mr: '15px',
+                                                        height: '85px',
+                                                        width: '25%',
+                                                        minWidth: '240px',
+                                                        boxShadow: 'none'
+                                                    }}>
+                                                    <CardContent sx={{ width: '100%', padding: '0px' }}>
+                                                        <Grid container direction='row' >
+                                                            <Grid item xs={4}>
+                                                                <Avatar sx={{ ml: '5px', width: 50, height: 50 }}>M</Avatar>
+                                                            </Grid>
+                                                            <Grid item xs={8} container direction='column' textAlign='start'>
+                                                                <Typography fontWeight='bold'>
+                                                                    {pet.nombre}
+                                                                </Typography>
+                                                                <Typography>
+                                                                    {pet.raza}
+                                                                </Typography>
+                                                            </Grid>
+                                                        </Grid>
+                                                    </CardContent>
+                                                </Card>
+                                            ))}
+                                        </Grid>
+                                    </div>
+                                    <div hidden={!isHidden}>
+                                        <Typography ml='15px' mt='8px' variant='body1'>Sin mascotas registradas.</Typography>
+                                    </div>
+                                </Typography>
+                            </div>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid >

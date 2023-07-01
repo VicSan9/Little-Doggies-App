dcd260b58a5838852447156dbf7aacf061c2ea56
Mercy09 <mercy.castro@correounivalle.edu.co>
2023-06-30 21:25:26 -0500

    Update services.controller.js

--- backend/src/controllers/services.controller.js
+++ backend/src/controllers/services.controller.js
@@ -26,11 +26,11 @@ const getServices = async (req, res, next) => {
 }
 
 const createServices = async (req, res, next) => {
-    const { nombre, categoria, descripcion } = req.body;
+    const { nombre, categoria, descripcion, estado } = req.body;
     try {
         const result = await pool.query
-            ('INSERT INTO servicios (nombre, categoria, descripcion) VALUES ($1, $2, $3) RETURNING *',
-                [nombre, categoria, descripcion]);                                 
+            ('INSERT INTO servicios (nombre, categoria, descripcion, estado) VALUES ($1, $2, $3, $4) RETURNING *',
+                [nombre, categoria, descripcion, estado]);                                 
         res.json(result.rows[0]);
     } catch (error){
         next(error);
@@ -54,11 +54,11 @@ const deleteServices = async (req, res, next) => {
 
 const updateServices = async (req, res, next) => {
     const { id } = req.params;
-    const { nombre, categoria, descripcion } = req.body;
+    const { nombre, categoria, descripcion, estado } = req.body;
     try {
         const result = await pool.query(
-            'UPDATE servicios SET nombre = $1, categoria = $2, descripcion = $3 WHERE svid = $4 RETURNING *',
-            [nombre, categoria, descripcion, id]);
+            'UPDATE servicios SET nombre = $1, categoria = $2, descripcion = $3, estado = $4 WHERE svid = $5 RETURNING *',
+            [nombre, categoria, descripcion, estado, id]);
         if (result.rows.length === 0)
             return res.status(404).json({
                 message: "Servicio no encontrado",

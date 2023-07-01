ce648dd23bc127ec6c7abc962eca546787b759b1
Víctor Alfonso Sánchez Lasso <svictorvf910@gmail.com>
2023-06-30 19:46:20 -0500

    Update ModifyInformation.js

--- frontend/src/components/ModifyInformation.js
+++ frontend/src/components/ModifyInformation.js
@@ -76,9 +76,6 @@ export default function ModifyInformation() {
             setErrorMessage('Correo electrónico ya registrado')
             return
         }
-
-
-
     }
 
     const handleClick = () => {

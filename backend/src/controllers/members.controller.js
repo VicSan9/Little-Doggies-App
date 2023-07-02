4f2abccca1dc8d16cea3626e8ac2a5ed97168662
Mercy09 <mercy.castro@correounivalle.edu.co>
2023-07-02 10:11:40 -0500

    Update members.controllers.js

--- backend/src/controllers/members.controller.js
+++ backend/src/controllers/members.controller.js
@@ -3,7 +3,17 @@ const pool = require('../db');
 const getAllMembers = async (req, res, next) => {
     try {
         const AllMembers = await pool.query
-            ('SELECT * FROM miembros  WHERE rol =$1', ['Trabajador']);
+            ('SELECT * FROM miembros');
+        res.json(AllMembers.rows);
+    } catch (error){ 
+        next(error);
+    }
+}
+
+const getAllMembers2 = async (req, res, next) => {
+    try {
+        const AllMembers = await pool.query
+            ('SELECT * FROM miembros WHERE rol = $1', ['Trabajador']);
         res.json(AllMembers.rows);
     } catch (error){ 
         next(error);
@@ -26,11 +36,11 @@ const getMember = async (req, res, next) => {
 }
 
 const  createMember = async (req, res, next) => {
-    const { usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto } = req.body;
+    const { usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto, estado } = req.body;
     try {
         const result = await pool.query
-            ('INSERT INTO miembros (usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
-                [usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto]);                                 
+            ('INSERT INTO miembros (usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto, estado) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
+                [usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto, estado]);                                 
         res.json(result.rows[0]);
     } catch (error){
         next(error);
@@ -54,11 +64,11 @@ const deleteMember = async (req, res, next) => {
 
 const updateMember = async (req, res, next) => {
     const { id } = req.params;
-    const { usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto } = req.body;
+    const { usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto, estado } = req.body;
     try {
         const result = await pool.query(
-            'UPDATE miembros SET usuario = $1, contraseña = $2, correo = $3, nombres = $4, apellidos = $5, telefono = $6, direccion = $7, rol = $8, foto = $9 WHERE mbid = $10 RETURNING *',
-            [usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto, id]);
+            'UPDATE miembros SET usuario = $1, contraseña = $2, correo = $3, nombres = $4, apellidos = $5, telefono = $6, direccion = $7, rol = $8, foto = $9 , estado =$10 WHERE mbid = $11 RETURNING *',
+            [usuario, contraseña, correo, nombres, apellidos, telefono, direccion, rol, foto,estado, id]);
         if (result.rows.length === 0)
             return res.status(404).json({
                 message: "Miembro no encontrado",
@@ -71,6 +81,7 @@ const updateMember = async (req, res, next) => {
 
 module.exports = {
     getAllMembers,
+    getAllMembers2,
     getMember,
     createMember,
     deleteMember,

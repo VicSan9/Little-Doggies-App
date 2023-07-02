c3ac3a8f728873382a058fd22101588394398ad7
Mercy09 <mercy.castro@correounivalle.edu.co>
2023-07-02 10:12:02 -0500

    Update members.routes.js

--- backend/src/routes/members.routes.js
+++ backend/src/routes/members.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { getAllMembers, getMember, createMember, deleteMember, updateMember } = require('../controllers/members.controller')
+const { getAllMembers, getMember, createMember, deleteMember, updateMember, getAllMembers2 } = require('../controllers/members.controller')
 
 const router = Router();
 
 router.get('/members', getAllMembers)
+
+router.get('/members2', getAllMembers2)
 
 router.get('/members/:id', getMember)
 

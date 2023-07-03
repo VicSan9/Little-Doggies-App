994a9cdec862c3f07fed8006a9902674d686c689
Víctor Alfonso Sánchez Lasso <svictorvf910@gmail.com>
2023-07-02 22:49:11 -0500

    Update userShoppingCard.js

--- frontend/src/components/userShoppingCard.js
+++ frontend/src/components/userShoppingCard.js
@@ -250,10 +250,10 @@ export default function UserShoppingCart() {
                                 container
                                 width='100%'
                                 borderBottom='1px solid #BABBBF'>
-                                <Grid container justifyContent='start' item xs={6} sm={6} lg={6} md={6} xl={6}>
+                                <Grid container justifyContent='start' item xs={6} sm={6} md={6} lg={6} xl={6}>
                                     <Typography mb='10px' ml='30px' fontWeight='bold'>Productos</Typography>
                                 </Grid>
-                                <Grid container justifyContent='end' item xs={6} sm={6} lg={6} md={6} xl={6}>
+                                <Grid container justifyContent='end' item xs={6} sm={6} md={6} lg={6} xl={6} sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'flex', xl: 'flex' } }}>
                                     <Typography mb='10px' mr='30px' fontWeight='bold'>Subtotal</Typography>
                                 </Grid>
                             </Grid>
@@ -287,8 +287,9 @@ export default function UserShoppingCart() {
                                         key={product.prid}
                                         container
                                         width='100%'
-                                        mb='20px'>
-                                        <Grid container justifyContent='start' item xs={6} sm={6} lg={6} md={6} xl={6}>
+                                        mb='20px'
+                                        sx={{ display: { xs: 'none', sm: 'flex', md: 'flex', lg: 'flex', xl: 'flex' } }}>
+                                        <Grid container justifyContent='start' item xs={12} sm={12} md={12} lg={6} xl={6} >
                                             <img
                                                 src={process.env.PUBLIC_URL + "/" + product.foto}
                                                 width='200px'
@@ -360,7 +361,102 @@ export default function UserShoppingCart() {
                                                 </Grid>
                                             </Grid>
                                         </Grid>
-                                        <Grid container alignContent='center' justifyContent='end' item xs={6} sm={6} lg={6} md={6} xl={6}>
+                                        <Grid
+                                            container
+                                            alignContent='center'
+                                            justifyContent='end'
+                                            item xs={0} sm={0} md={0} lg={6} xl={6}
+                                            sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'flex', xl: 'flex' } }}>
+                                            <Typography mr='20px'>{'$' + product.precio * product.count}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                ))}
+                                {products.map((product) => (
+                                    <Grid
+                                        key={product.prid}
+                                        container
+                                        width='100%'
+                                        mb='20px'
+                                        sx={{ display: { xs: 'flex', sm: 'none', md: 'none', lg: 'none', xl: 'none' } }}>
+                                        <Grid container justifyContent='start' item xs={12} sm={12} md={12} lg={6} xl={6} >
+                                            <img
+                                                src={process.env.PUBLIC_URL + "/" + product.foto}
+                                                width='155px'
+                                                alt="foto">
+                                            </img>
+                                            <Grid>
+                                                <Typography mt='10px' ml='10px'>{product.nombre}</Typography>
+                                                <Typography ml='10px'>{'$' + product.precio}</Typography>
+                                                <Grid mt='100px' container direction='row'>
+                                                    <Box
+                                                        ml='10px'
+                                                        width='25px'
+                                                        height='30px'
+                                                        component={Button}
+                                                        value={product.prid}
+                                                        onClick={handleClickRes}
+                                                        sx={{
+                                                            color: '#000000',
+                                                            textDecoration: 'none',
+                                                            textTransform: 'none',
+                                                            minWidth: '40px',
+                                                            boxShadow: 'none',
+                                                            padding: '0px',
+                                                            backgroundColor: '#F5F5F5',
+                                                            borderRadius: '0px',
+                                                        }}> -
+                                                    </Box>
+                                                    <Box
+                                                        component={Grid}
+                                                        container
+                                                        width='25px'
+                                                        height='30px'
+                                                        alignContent='center'
+                                                        justifyContent='center'
+                                                        sx={{
+                                                            boxShadow: 'none',
+                                                            padding: '0px',
+                                                            backgroundColor: '#F5F5F5',
+                                                        }}> {product.count}
+                                                    </Box>
+                                                    <Box
+                                                        width='25px'
+                                                        height='30px'
+                                                        value={product.prid}
+                                                        component={Button}
+                                                        onClick={handleClickSum}
+                                                        sx={{
+                                                            color: '#000000',
+                                                            textDecoration: 'none',
+                                                            textTransform: 'none',
+                                                            minWidth: '40px',
+                                                            boxShadow: 'none',
+                                                            padding: '0px',
+                                                            backgroundColor: '#F5F5F5',
+                                                            borderRadius: '0px',
+                                                        }}> +
+                                                    </Box>
+                                                    <Button
+                                                        id={product.prid}
+                                                        onClick={handleClickDelete}
+                                                        variant="outlined"
+                                                        color='error'
+                                                        sx={{
+                                                            ml: '10px',
+                                                            borderRadius: '20px',
+                                                            borderColor: '#CD0227',
+                                                            textTransform: 'none',
+                                                        }}>Eliminar
+                                                    </Button>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid
+                                            container
+                                            alignContent='center'
+                                            justifyContent='end'
+                                            item xs={0} sm={0} md={0} lg={6} xl={6}
+                                            sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'flex', xl: 'flex' } }}>
                                             <Typography mr='20px'>{'$' + product.precio * product.count}</Typography>
                                         </Grid>
                                     </Grid>

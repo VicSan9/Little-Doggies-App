2a81e3793d3b58a593ad1988f2af2758a5baa46b
Víctor Alfonso Sánchez Lasso <svictorvf910@gmail.com>
2023-07-04 00:15:25 -0500

    Update services.routes.js

--- backend/src/routes/services.routes.js
+++ backend/src/routes/services.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { getAllServices, getServices, createServices, deleteServices, updateServices } = require('../controllers/services.controller')
+const { getAllServices, getServices, createServices, deleteServices, updateServices, getServices2 } = require('../controllers/services.controller')
 
 const router = Router();
 
 router.get('/services', getAllServices)
+
+router.get('/services2', getServices2)
 
 router.get('/services/:id', getServices)
 
